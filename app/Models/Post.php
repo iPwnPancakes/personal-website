714c9310e3f0c41a062a52e3b9933f8ce78ac293
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-07-10 23:49:35 -0500

    Created model/migration for posts

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Post extends Model
+{
+    use HasSlug;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(50)
+            ->skipGenerateWhen(fn() => $this->state === 'draft')
+            ->doNotGenerateSlugsOnUpdate();
+    }
+}
---
+++ database/migrations/2024_07_11_041619_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public $withinTransaction = true;
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('title');
+            $table->string('blade-file');
+            $table->enum('state', ['published', 'draft']);
+            $table->string('slug', 255)->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
