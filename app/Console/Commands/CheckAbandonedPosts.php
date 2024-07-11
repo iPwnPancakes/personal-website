e4de6d6739c254bec4438554b6b804de41e92a2d
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-07-11 00:35:53 -0500

    Created command to check for posts that have missing blade files

---
+++ app/Console/Commands/CheckAbandonedPosts.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Post;
+use App\Models\PostStates;
+use Illuminate\Console\Command;
+
+class CheckAbandonedPosts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'posts:abandoned';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $posts = Post::where('state', PostStates::PUBLISHED)->get();
+
+        $postsWithMissingBladeFiles = $posts
+            ->filter(fn ($post) => ! view()->exists($post->blade_file))
+            ->map(fn ($post) => ['ID' => $post->id, 'Title' => $post->title, 'Blade file' => $post->blade_file]);
+
+        if (! empty($postsWithMissingBladeFiles)) {
+            $this->info('The following posts have missing blade files:');
+            $this->table(['ID', 'Title', 'Blade file'], $postsWithMissingBladeFiles);
+
+            return 1;
+        }
+
+        return 0;
+    }
+}
